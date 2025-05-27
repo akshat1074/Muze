d9ee4080b8a4f228b114a092bff1efdeef55c889
akshat_1074 <valaniakshat1074@gmail.com>
2025-05-27 17:02:57 +0530

    schema

---
+++ app/app/api/streams/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+import {z} from "zod";
+
+const CreateStreamSchema = z.object({
+     creatorId:z.string(),
+     url:z.string()
+})
+
+
+export async function POST(req:NextRequest){
+
+}
