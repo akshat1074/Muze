e18c959a67c2a1be9c7d2e5573e7c2fcfce8ff6a
akshat_1074 <valaniakshat1074@gmail.com>
2025-05-28 23:36:08 +0530

    apiroutes

--- app/app/api/streams/route.ts
+++ app/app/api/streams/route.ts
@@ -1,5 +1,7 @@
-import { NextRequest } from "next/server";
+import { NextRequest ,NextResponse} from "next/server";
 import {z} from "zod";
+import {prismaClient} from "@/app/lib/db"
+
 
 const CreateStreamSchema = z.object({
      creatorId:z.string(),
@@ -8,5 +10,16 @@ const CreateStreamSchema = z.object({
 
 
 export async function POST(req:NextRequest){
-
+     try{
+        const data = CreateStreamSchema.parse(await req.json)
+        prismaClient.stream.create({
+             
+        })
+     } catch(e){
+        return NextResponse.json({
+        message:"Error while adding a stream"
+        },{
+            status:411
+        })
+     }
 }
